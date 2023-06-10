85a48613a897509f6c495cf6d71443897eb9e03d
Antonio-Vel <veleza@bxscience.edu>
2023-06-10 02:45:18 -0400

    Added basic formation system

--- Assets/SelectionActions.cs
+++ Assets/SelectionActions.cs
@@ -4,45 +4,126 @@ using UnityEngine;
 
 public class SelectionActions : MonoBehaviour
 {
-    public Dictionary<string, GameObject> selected;
-    Vector2 mousepos;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Dictionary<string, GameObject> selected;
+	Vector3 mousepos;
+	Vector3 origin;
+	bool LoR = false;
+	const float MOVE = .5F;
+	List<float> distances = new();
+	Dictionary<float, Movable> movables = new();
+	// Update is called once per frame
+	void Update()
+	{
+		selected = Selection.getSelected();
 
-    // Update is called once per frame
-    void Update()
-    {
-        selected = Selection.getSelected();
+		if (selected.Count == 0)
+		{ return; }
 
-        if (selected.Count == 0)
-        { return; }
+		if (Input.GetKey("p") || Input.GetKeyUp("p")) 
+		{
+			Move(selected);
+		}
+  
+	}
+		//Code Museum
 
-        mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (Input.GetKeyDown("p"))
-        {
-            foreach (KeyValuePair<string, GameObject> pair in selected)
-            {
-                print("howdy");
+		//mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		//foreach (KeyValuePair<string, GameObject> pair in selections)
+		//{
+		//    if (pair.Value.TryGetComponent<Movable>(out Movable component))
+		//    {
+		//        print($"{pair.Key} is moving to {Camera.main.ScreenToWorldPoint(Input.mousePosition)}!");
+		//        if (Input.GetKey("w"))
+		//        {
+		//            component.AddToMoveQueue(mousepos);
+		//        }
+		//        else
+		//        {
+		//            component.MoveTo(mousepos);
+		//        }
+		//    }
+		//    int rand = Random.Range(0, 360);
+		//    mousepos = new Vector2((float)(mousepos.x + .5 * Mathf.Cos(rand)), (float)(mousepos.y + .5 * Mathf.Sin(rand)));
+		//}
 
-                if (pair.Value.TryGetComponent<Movable>(out Movable component))
-                {
-                    print($"{pair.Key} is moving to {Camera.main.ScreenToWorldPoint(Input.mousePosition)}!");
-                    if (Input.GetKey("w"))
-                    {
-                        component.AddToMoveQueue(mousepos);
-                    }
-                    else
-                    {
-                        component.MoveTo(mousepos);
-                    }
-                }
-                int rand = Random.Range(0, 360);
+	void Move(Dictionary<string, GameObject> selections)
+	{
+		
 
-                mousepos = new Vector2((float)(mousepos.x + .5 * Mathf.Cos(rand)), (float)(mousepos.y + .5 * Mathf.Sin(rand)));
-            }
-        }
-    }
+		if (Input.GetKeyDown("p"))
+		{
+			origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			foreach (KeyValuePair<string, GameObject> pair in selections)
+			{
+				if (pair.Value.TryGetComponent<Movable>(out Movable comp))
+				{
+					movables.Add(Vector2.Distance((Vector2)origin, pair.Value.GetComponent<Rigidbody2D>().position),comp);
+					distances.Add(Vector2.Distance((Vector2)origin, pair.Value.GetComponent<Rigidbody2D>().position));
+				}
+			}
+			distances.Sort();
+		}
+		mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	 
+		//Sort by movable & distance from mousepos
+		//Find position of mouse relative to original position in order to find rotation of formation
+		Vector3 delta = mousepos - origin;
+		float rotation = Mathf.Asin(delta.y / delta.magnitude);
+		//Create a formation of positions with rotation in mind
+		Vector2[] formation = new Vector2[movables.Count];
+		Vector2 movefactor = new Vector2(MOVE * Mathf.Cos(rotation), MOVE * Mathf.Sin(rotation));
+		if (formation.Length % 2 == 0)
+		{
+			Vector2 smallconstant = movefactor / 2;
+			formation[0] = (Vector2)origin + movefactor / 2;
+			formation[1] = (Vector2)origin + movefactor / -2;
+			for (int i = 2; i < formation.Length; i++)
+			{
+				formation[i] = (Vector2)origin + movefactor + smallconstant ;
+				if (LoR)
+				{
+					movefactor += movefactor;
+				}
+				LoR = !LoR;
+				movefactor *= -1;
+				smallconstant *= -1;
+			}
+		}
+		else
+		{
+			formation[0] = (Vector2)origin;
+			for (int i = 1; i < formation.Length; i++)
+			{
+				formation[i] = (Vector2)origin + movefactor;
+				if (LoR)
+				{
+					movefactor += movefactor;
+				}
+				LoR = !LoR;
+				movefactor *= -1;
+			}
+		}
+		//Send out formation details on mouseup
+		if (Input.GetKeyUp("p"))
+		{
+			for (int i = 0; i < distances.Count; i++)
+			{
+				movables.TryGetValue(distances[i], out Movable m);
+				if (Input.GetKey("w"))
+				{
+					m.AddToMoveQueue(formation[i]);
+				}
+				else
+				{
+					m.MoveTo(formation[i]);
+				}
+			}
+
+			movables.Clear();
+			distances.Clear();
+		}
+	}
 }
+
+
+
