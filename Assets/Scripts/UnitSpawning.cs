ae467661435ba893b2a74b549fd1e15ffc8a8a52
Antonio-Vel <alejay831@gmail.com>
2023-08-17 22:55:00 -0400

    Basic Spawning System Implemented

--- Assets/Scripts/UnitSpawning.cs
+++ Assets/Scripts/UnitSpawning.cs
@@ -2,7 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitSpawning : MonoBehaviour
+public class Unit : MonoBehaviour
 {
-    Unit 
+	static Dictionary<string, UnitScriptableObject> unitDictionary = new Dictionary<string, UnitScriptableObject>();
+	private void Start()
+	{
+		UnitScriptableObject[] UnitArray = Resources.LoadAll<UnitScriptableObject>("Scriptable Obect");
+		foreach (UnitScriptableObject obj in UnitArray)
+		{
+			unitDictionary.Add(obj.name, obj);
+		}
+	}
+
+	static void Spawn(string name,Vector3 pos)
+	{
+		if (!unitDictionary.TryGetValue(name, out UnitScriptableObject query))
+		{ print($"NAME \"{name}\" NOT FOUND"); return; }
+
+		GameObject obj = Instantiate(query.prefab, pos, Quaternion.identity);
+			
+	}
+
+
+	int health;
+	float movespeed;
+	float attackSpeed;
+	float attackRange;
+	string description;
+	GameObject unitObject;
+
+	Unit(UnitScriptableObject template, GameObject linkedObj)
+	{
+		health = template.health;
+		movespeed = template.movespeed;
+		attackSpeed = template.attackSpeed;
+		attackRange = template.attackRange;
+		description = template.description;
+		unitObject = linkedObj;
+	}
 }
